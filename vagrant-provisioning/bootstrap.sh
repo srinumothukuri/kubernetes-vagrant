88c878a93a8553da04fefb08c6c1c1899cf4bb7d
srinumothukuri <m.srinivas2374@gmail.com>
2024-04-09 15:34:17 +0530

    base image and ipaddress range changed

--- vagrant-provisioning/bootstrap.sh
+++ vagrant-provisioning/bootstrap.sh
@@ -64,7 +64,7 @@ echo "export TERM=xterm" >> /etc/bash.bashrc
 
 echo "[TASK 10] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
-172.16.16.100   kmaster.example.com     kmaster
-172.16.16.101   kworker1.example.com    kworker1
-172.16.16.102   kworker2.example.com    kworker2
+192.168.56.70   kmaster.example.com     kmaster
+192.168.56.71   kworker1.example.com    kworker1
+192.168.56.72   kworker2.example.com    kworker2
 EOF
--- vagrant-provisioning/bootstrap_kmaster.sh
+++ vagrant-provisioning/bootstrap_kmaster.sh
@@ -4,7 +4,7 @@ echo "[TASK 1] Pull required containers"
 kubeadm config images pull >/dev/null
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
+kubeadm init --apiserver-advertise-address=192.168.56.70 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
 
 echo "[TASK 3] Deploy Calico network"
 kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml >/dev/null
